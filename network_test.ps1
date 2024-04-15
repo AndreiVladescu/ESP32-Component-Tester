5696afdc95321ddc351386e80ac57e56f0348a6a
Andrei Vladescu <52881237+AndreiVladescu@users.noreply.github.com>
2024-04-15 17:31:30 +0300

    Delete network_test.ps1

--- network_test.ps1
+++
@@ -1,6 +0,0 @@
-﻿
-$link_speed = (Get-NetAdapter | Where-Object Name -eq Ethernet | Select -expand LinkSpeed).toString()
-if (!$link_speed.Contains("Gbps")){
-    Write-Output("Re-insert Ethernet Cable")
-	Read-Host -Prompt "Press Enter to exit"
-}
