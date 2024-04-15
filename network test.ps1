f9d1cc8287fce99e405f8844b6856a483eb5e356
Andrei Vladescu <52881237+AndreiVladescu@users.noreply.github.com>
2024-04-15 17:24:58 +0300

    Add files via upload
    Persistent User Prompt

---
+++ network test.ps1
@@ -0,0 +1,6 @@
+﻿
+$link_speed = (Get-NetAdapter | Where-Object Name -eq Ethernet | Select -expand LinkSpeed).toString()
+if (!$link_speed.Contains("Gbps")){
+    Write-Output("Re-insert Ethernet Cable")
+	Read-Host -Prompt "Press Enter to exit"
+}
