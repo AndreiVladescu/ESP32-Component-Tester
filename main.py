9c710af8e263d8090c393c25bfb36b8a27d03f68
Andrei Vladescu <52881237+AndreiVladescu@users.noreply.github.com>
2024-04-29 12:23:46 +0300

    Added resistance measurement stubs

--- main.py
+++ main.py
@@ -25,6 +25,9 @@ class TestPoint:
         self.r0 = Pin(r0_pin, Pin.IN)
         self.r1 = Pin(r1_pin, Pin.IN)
         self.r2 = Pin(r2_pin, Pin.IN)
+        self.r0_status = 0
+        self.r1_status = 0
+        self.r2_status = 0
 
     def get_uv(self):
         return self.adc.read_uv()
@@ -32,44 +35,67 @@ class TestPoint:
     def get_v(self):
         return self.get_uv() / 10**6
 
+    def get_status(self):
+        return 'R0: {0}, R1: {1}, R2: {2}'.format(self.r0_status, self.r1_status, self.r2_status)
+    
     def set_r0_high(self):
         self.r0 = Pin(self.r0_pin, Pin.OUT)
         self.r0.on()
+        self.r0_status = 1
+        self.set_r1_floating()
+        self.set_r2_floating()
 
     def set_r0_low(self):
         self.r0 = Pin(self.r0_pin, Pin.OUT)
         self.r0.off()
+        self.r0_status = -1
+        self.set_r1_floating()
+        self.set_r2_floating()
 
     def set_r0_floating(self):
+        self.r0_status = 0
         self.r0 = Pin(self.r0_pin, Pin.IN)
 
     def set_r1_high(self):
         self.r1 = Pin(self.r1_pin, Pin.OUT)
         self.r1.on()
+        self.r1_status = 1
+        self.set_r0_floating()
+        self.set_r2_floating()
 
     def set_r1_low(self):
         self.r1 = Pin(self.r1_pin, Pin.OUT)
         self.r1.off()
+        self.r1_status = -1
+        self.set_r0_floating()
+        self.set_r2_floating()
 
     def set_r1_floating(self):
+        self.r1_status = 0
         self.r1 = Pin(self.r1_pin, Pin.IN)
 
     def set_r2_high(self):
         self.r2 = Pin(self.r2_pin, Pin.OUT)
         self.r2.on()
+        self.r2_status = 1
+        self.set_r1_floating()
+        self.set_r2_floating()
 
     def set_r2_low(self):
         self.r2 = Pin(self.r2_pin, Pin.OUT)
         self.r2.off()
+        self.r2_status = -1
+        self.set_r1_floating()
+        self.set_r2_floating()
 
     def set_r2_floating(self):
+        self.r2_status = 0
         self.r2 = Pin(self.r2_pin, Pin.IN)
+
 
 tp1, tp2, tp3 = None, None, None
 
 ## Aux functions
-
-
 def debug(message):
     if debug_check:
         print(message)
@@ -85,15 +111,29 @@ def init_pins():
     tp3 = TestPoint(adc_tp3, tp3_pins[0], tp3_pins[1], tp3_pins[2])
 
 
-def measure_resistance(tp_x, tp_y):
+def measure_resistance_680(tp_x, tp_y):
+    tp_x.set_r0_low()
+    tp_y.set_r1_high()
+    sleep(0.001)
     print(tp_x.get_v())
     print(tp_y.get_v())
 
+def measure_resistance_470k(tp_x, tp_y):
+    tp_x.set_r0_low()
+    tp_y.set_r2_high()
+    sleep(0.001)
+    print(tp_x.get_v())
+    print(tp_y.get_v())
 
+def measure_resistance(tp_x, tp_y):
+    print("Before")
+    measure_resistance_680(tp_x, tp_y)
+    measure_resistance_470k(tp_x, tp_y)
+    
 def measure_phase():
     global tp1, tp2, tp3
     measure_resistance(tp1, tp2)
-
+    measure_resistance(tp2, tp1)
 
 def main():
     init_pins()
